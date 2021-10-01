402acad8e302dcf0171265ecc85b733f71be07fe
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-01 10:51:26 +0530

    The Player rools the die ti get number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,5 +2,7 @@ package com.campany;
 public class SnakeAndLadder {
      public static void main(String[] args) {
      System.out.println( "Snake and Ladder game played with single player at start postion 0");
+     int diceNumber=(int) (Math.floor(Math.random() * 10) % 6 +1);
+     System.out.println( "Dice On Number: " +diceNumber);
      }
 }
