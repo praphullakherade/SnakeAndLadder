0051846310ce7d6b843af1677d0ed81eebaa4320
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-01 10:47:36 +0530

    Sanke and Ladder game played with single player at start position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,6 @@
 package com.campany;
 public class SnakeAndLadder {
      public static void main(String[] args) {
-     System.out.println( "Snake and Ladder game");
+     System.out.println( "Snake and Ladder game played with single player at start postion 0");
      }
 }
