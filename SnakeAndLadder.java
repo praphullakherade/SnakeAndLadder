953baf9f0ef42ed671f3e2cf00e18d9e4923c75f
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-07 19:37:20 +0530

    Winning Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,24 +1,32 @@
 package com.campany;
 public class SnakeAndLadder {
      public static void main(String[] args) {
-     System.out.println( "Snake and Ladder game played with single player at start postion 0");
-     int diceNumber=(int) (Math.floor(Math.random() * 10) % 6 +1);
+     System.out.println( "Welcome To Snake And Ladder Simulator" );
      int startPosition = 0;
-     int endPosition = 100;
-     System.out.println( "Dice On Number: " +diceNumber);
-     int checkOption = (int) (Math.floor((Math.random() * 10) %3));
-		switch(checkOption){
-			case 1:
-			startPosition += 0;
-			break;
-			case 2:
-			startPosition += diceNumber;
-			break;
-			default:
-			startPosition -= diceNumber;
-		}
-		System.out.println("option Number:" +checkOption);
-		System.out.println("Position of the player: " +startPosition);
+		int finalPosition = 100;
+		System.out.println("stating position of player is " + startPosition);
+		while(startPosition <= finalPosition)
+		{
+			int diceValue = (int) Math.floor((Math.random() * 10) %6) + 1;
+			System.out.println("dice value is " + diceValue);
+			int checkOption = (int) Math.floor((Math.random() * 10) %3);
+			switch(checkOption){
+				case 1:
+				startPosition += 0;
+				break;
+				case 2:
+				startPosition += diceValue;
+				break;
+				default:
+				startPosition -= diceValue;
+			}
+			if(startPosition < 0){
+			startPosition = 0;
+			}
+			System.out.println("option value is " +checkOption);
+		
 			
+			System.out.println("Position of the player is at " +startPosition);
+		}
      }
 }
